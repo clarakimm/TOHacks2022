ef7cded63429cfd4210c036a4892d8df178d847b
Ethan Hsu <ethanhsu128@gmail.com>
2022-05-28 14:45:52 -0700

    add api

--- review_analysis.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title('An easy way to analyze YouTube video reviews')
-st.text_input ('Enter URL') 
---
+++ src/review_analysis.py
@@ -0,0 +1,67 @@
+import streamlit as st
+from configure import auth_key
+from pytube import YouTube
+import os
+import sys
+import time
+import requests
+from time import sleep
+from zipfile import ZipFile
+from pprint import pprint
+
+def read_file(filename, chunk_size=5242880):
+    with open(filename, 'rb') as _file:
+        while True:
+            data = _file.read(chunk_size)
+            if not data:
+                break
+            yield data
+
+video = YouTube('https://www.youtube.com/watch?v=jNQXAC9IVRw')
+yt = video.streams.get_audio_only()
+yt.download()
+
+current_dir = os.getcwd()
+for file in os.listdir(current_dir):
+    if file.endswith(".mp4"):
+        mp4_file = os.path.join(current_dir, file)
+        # print(mp4_file)
+
+filename = mp4_file
+headers = {'authorization': auth_key}
+response = requests.post('https://api.assemblyai.com/v2/upload', headers=headers, data=read_file(filename))
+
+audio_url = response.json()['upload_url']
+# print(audio_url)
+
+endpoint = "https://api.assemblyai.com/v2/transcript"
+json = {
+    "audio_url": audio_url,
+    "sentiment_analysis": True
+}
+headers = {
+    "authorization": auth_key,
+    "content-type": "application/json"
+}
+transcript_input_response = requests.post(endpoint, json=json, headers=headers)
+
+# print(transcript_input_response.json())
+
+transcript_id = transcript_input_response.json()["id"]
+endpoint = f"https://api.assemblyai.com/v2/transcript/{transcript_id}"
+
+headers = {
+    "authorization": auth_key,
+}
+
+transcript_output_response = requests.get(endpoint, headers=headers)
+while transcript_output_response.json()['status'] != 'completed':
+    sleep(5)
+    transcript_output_response = requests.get(endpoint, headers=headers)
+
+pprint(transcript_output_response.json()["sentiment_analysis_results"])
+# print(transcript_output_response.json()["text"])
+
+
+# st.title('An easy way to analyze YouTube video reviews')
+# st.text_input ('Enter URL') 
