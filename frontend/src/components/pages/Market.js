f69e2a3305ea16b996c60222511f5b2af3e54a2b
Leo Lau <leo233257@gmail.com>
2022-05-29 02:11:58 -0400

    Commit

--- frontend/src/components/pages/Market.js
+++ frontend/src/components/pages/Market.js
@@ -9,6 +9,41 @@ const Market = ({ page }) => {
       cost: "$400",
       image: "iPhone11.png",
     },
+    {
+      name: "iPhone X",
+      cost: "$300",
+      image: "iphonex.png",
+    },
+    {
+      name: "iPhone 12",
+      cost: "$600",
+      image: "iPhone12.png",
+    },
+    {
+      name: "Huawei P30 Pro",
+      cost: "$450",
+      image: "huaweip30pro.png",
+    },
+    {
+      name: "Huawei P40 Pro Plus",
+      cost: "$700",
+      image: "huaweiP40ProPlus.png",
+    },
+    {
+      name: "Samsung Galaxy S10",
+      cost: "$200",
+      image: "s10.png",
+    },
+    {
+      name: "Samsung Galaxy S20",
+      cost: "$500",
+      image: "s20.png",
+    },
+    {
+      name: "Samsung Galaxy S21 Ultra",
+      cost: "$800",
+      image: "s21ultra.png",
+    }
   ];
 
   return (
