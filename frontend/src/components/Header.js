9621abdecf0f9bf6e9bd779c0c824afebb12f9e3
emily-333 <emilyarose1@outlook.com>
2022-05-28 22:51:47 -0400

    changes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,29 +4,39 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <section className="header">
-      <section className="header-top">
-        <section className="header_logo">
-          <Link to="/" className="logo">
-            LOGO
-          </Link>
-        </section>
-        <section className="header_navbar">
-          <section className="header-top__navigation">
-            <Link to="/search" className="navbar-item">
-              SEARCH
-            </Link>
-            <Link to="/market" className="navbar-item">
-              MARKETPLACE
-            </Link>
-            <Link to="/map" className="navbar-item">
-              MAP
-            </Link>
-          </section>
-          <hr className="header-top_seperator" />
-        </section>
-      </section>
-    </section>
+    <header className="header">
+      <Link to="/" className="logo">
+        LOGO
+      </Link>
+      <div className="nav-container">
+        <Link to="/search" className="navbar-item">SEARCH</Link>
+        <Link to="/market" className="navbar-item">MARKETPLACE</Link>
+        <Link to="/map" className="navbar-item">MAP</Link>
+      </div>
+    </header>
+    // <section className="header">
+    //   <section className="header-top">
+    //     <section className="header_logo">
+    //       <Link to="/" className="logo">
+    //         LOGO
+    //       </Link>
+    //     </section>
+    //     <section className="header_navbar">
+    //       <section className="header-top__navigation">
+    //         <Link to="/search" className="navbar-item">
+    //           SEARCH
+    //         </Link>
+    //         <Link to="/market" className="navbar-item">
+    //           MARKETPLACE
+    //         </Link>
+    //         <Link to="/map" className="navbar-item">
+    //           MAP
+    //         </Link>
+    //       </section>
+    //       <hr className="header-top_seperator" />
+    //     </section>
+    //   </section>
+    // </section>
   );
 };
 
