59910b580a4711b0ac901827404b2ae2e438613d
emily-333 <emilyarose1@outlook.com>
2022-05-28 22:25:40 -0400

    test

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,24 +1,23 @@
 import React from "react";
 import './Header.css';
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <section className="header">
       <section className="header-top">
-        <section className="header_logo">
-          <Link to="/" className="logo">LOGO</Link>
+        <section className="header-top_logo">
+          <a href="/" className="header-logo">LOGO</a>
         </section>
-        <section className="header_navbar">
-          <section className="header-top__navigation">
-            <Link to="/" className="navbar-item">SEARCH</Link>
-            <Link to="/" className="navbar-item">MARKETPLACE</Link>
-            <Link to="/" className="navbar-item">MAP</Link>
+        <section className="header-top_navbar">
+          <section className="header-top_navigation">
+            <a href ="/" className="navbar-item">SEARCH</a>
+            <a href ="/" className="navbar-item">MARKETPLACE</a>
+            <a href ="/" className="navbar-item">MAP</a>
           </section>
           <hr className="header-top_seperator"/>
         </section>
-        </section>
-      
+      </section>
     </section>
   );
 };
