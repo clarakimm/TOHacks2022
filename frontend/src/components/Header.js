ad838c313153c8f045b36b32fffca14501983e6e
emily-333 <emilyarose1@outlook.com>
2022-05-28 22:01:07 -0400

    message

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,25 @@
 import React from "react";
+import './Header.css';
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="header">
-      <div>Search</div>
-      <div>Market Place</div>
-      <div>Map</div>
-    </div>
+    <section className="header">
+      <section className="header-top">
+        <section className="header_logo">
+          <Link to="/" className="logo">LOGO</Link>
+        </section>
+        <section className="header_navbar">
+          <section className="header-top__navigation">
+            <Link to="/" className="navbar-item">SEARCH</Link>
+            <Link to="/" className="navbar-item">MARKETPLACE</Link>
+            <Link to="/" className="navbar-item">MAP</Link>
+          </section>
+          <hr className="header-top_seperator"/>
+        </section>
+        </section>
+      
+    </section>
   );
 };
 
