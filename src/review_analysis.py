8da56c770d376282f1c02e5b7c6b02b0c3a25564
clara <clarakim113@gmail.com>
2022-05-28 20:32:58 -0400

    updates

--- src/review_analysis.py
+++ src/review_analysis.py
@@ -8,6 +8,9 @@ import requests
 from time import sleep
 from zipfile import ZipFile
 from pprint import pprint
+import timeit
+
+start = timeit.default_timer()
 
 def read_file(filename, chunk_size=5242880):
     with open(filename, 'rb') as _file:
@@ -17,46 +20,53 @@ def read_file(filename, chunk_size=5242880):
                 break
             yield data
 
-video = YouTube('https://www.youtube.com/watch?v=jNQXAC9IVRw')
-yt = video.streams.get_audio_only()
-yt.download()
+def getSentiment (links):
+    video = YouTube(links)
+    yt = video.streams.get_audio_only()
+    yt.download()
 
-current_dir = os.getcwd()
-for file in os.listdir(current_dir):
-    if file.endswith(".mp4"):
-        mp4_file = os.path.join(current_dir, file)
-        # print(mp4_file)
+    current_dir = os.getcwd()
+    for file in os.listdir(current_dir):
+        if file.endswith(".mp4"):
+            mp4_file = os.path.join(current_dir, file)
+            # print(mp4_file)
 
-filename = mp4_file
-headers = {'authorization': auth_key}
-response = requests.post('https://api.assemblyai.com/v2/upload', headers=headers, data=read_file(filename))
+    filename = mp4_file
+    headers = {'authorization': auth_key}
+    response = requests.post('https://api.assemblyai.com/v2/upload', headers=headers, data=read_file(filename))
 
-audio_url = response.json()['upload_url']
-# print(audio_url)
+    audio_url = response.json()['upload_url']
+    # print(audio_url)
 
-endpoint = "https://api.assemblyai.com/v2/transcript"
-json = {
-    "audio_url": audio_url,
-    "sentiment_analysis": True
-}
-headers = {
-    "authorization": auth_key,
-    "content-type": "application/json"
-}
-transcript_input_response = requests.post(endpoint, json=json, headers=headers)
+    endpoint = "https://api.assemblyai.com/v2/transcript"
+    json = {
+        "audio_url": audio_url,
+        "sentiment_analysis": True
+    }
+    headers = {
+        "authorization": auth_key,
+        "content-type": "application/json"
+    }
+    transcript_input_response = requests.post(endpoint, json=json, headers=headers)
 
-# print(transcript_input_response.json())
+    # print(transcript_input_response.json())
 
-transcript_id = transcript_input_response.json()["id"]
-endpoint = f"https://api.assemblyai.com/v2/transcript/{transcript_id}"
+    transcript_id = transcript_input_response.json()["id"]
+    endpoint = f"https://api.assemblyai.com/v2/transcript/{transcript_id}"
 
-headers = {
-    "authorization": auth_key,
-}
+    headers = {
+        "authorization": auth_key,
+    }
 
-transcript_output_response = requests.get(endpoint, headers=headers)
-while transcript_output_response.json()['status'] != 'completed':
-    sleep(1)
     transcript_output_response = requests.get(endpoint, headers=headers)
+    while transcript_output_response.json()['status'] != 'completed':
+        sleep(1)
+        transcript_output_response = requests.get(endpoint, headers=headers)
 
-pprint(transcript_output_response.json()["sentiment_analysis_results"])
+    pprint(transcript_output_response.json()["sentiment_analysis_results"])
+
+getSentiment('https://www.youtube.com/watch?v=8-Vj9dOzD7Y')
+
+stop = timeit.default_timer()
+print ('Time: ', stop - start)
+
