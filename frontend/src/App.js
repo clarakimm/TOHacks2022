e96b85b600b84d37d0c484730a40e840d441c42b
Ethan Hsu <ethanhsu128@gmail.com>
2022-05-28 19:06:52 -0700

    pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,42 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Form from "./components/Form";
 import Header from "./components/Header";
+import Test from "./components/Test";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  const [something, setSomething] = useState("");
+  const [something, setSomething] = useState("aaa");
 
-  fetch("http://127.0.0.1:5000/")
-    .then((res) => res.json())
-    .then((data) => setSomething(data));
+  const fetchData = () => {
+    fetch("http://127.0.0.1:5000/")
+      .then((res) => res.json())
+      .then((data) => {
+        setSomething(data.data);
+        console.log("hi");
+      }, []);
+  };
+  useEffect(() => {
+    fetchData();
+  });
 
   return (
-    <div className="App">
-      {something}
-      <Header></Header>
-      <Form />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Test page={home} />
+        </Route>
+        <Route exact path="/search">
+          <Test page={search} />
+        </Route>
+        <Route exact path="/market">
+          <Test page={market} />
+        </Route>
+        <Route exact path="/map">
+          <Test page={map} />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ frontend/src/components/Test.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Test = ({ page }) => {
+  return <div className="test">the page is: {page}</div>;
+};
+
+export default Test;
